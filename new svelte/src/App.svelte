2099f76846874e8d9c524a904fea39c504797006
Digital <mollyjazzharry8@gmail.com>
2025-01-25 17:41:58 +1100

    added new svelte

---
+++ new svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ModeSwitcher from './lib/ModeSwitcher.svelte';
+</script>
+<ModeSwitcher />
+<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[#d66a6f] text-2xl md:Zen-Kaku-Gothic-New">
+	<h1 class='flex items-center justify-center text-5xl'>Welcome to felixargyle.dev!</h1>
+	<br>
+	<p>The personal website of dgtlnght!</p>
+  </div>
