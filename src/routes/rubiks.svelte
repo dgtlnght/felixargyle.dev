fb48275ffb20e47b4b251b71b1de133aea399c69
Digital <85112522+dgtlnght@users.noreply.github.com>
2025-03-01 22:49:15 +1100

    wadawda
    awdawda

--- src/routes/rubiks.svelte
+++ src/routes/rubiks.svelte
@@ -1,9 +1,11 @@
-<h1>Mitch's best time:</h1>
+<body class="bg-cd5c5c">
+    <h1>Mitch's best time:</h1>
 <br>
-<h1>56.56</h1>
+    <h1>56.56</h1>
 <br>
 <br>
 <br>
-<h1>Oliver's best time</h1>
+    <h1>Oliver's best time</h1>
 <br>
-<h1>56.51</h1>
+    <h1>56.51</h1>
+</body>
