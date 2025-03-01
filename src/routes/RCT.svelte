5c798fc6126d7ee664bf37e60361737c701bea43
Digital <85112522+dgtlnght@users.noreply.github.com>
2025-03-01 23:46:45 +1100

    rea
     ewa

--- src/routes/RCT.svelte
+++ src/routes/RCT.svelte
@@ -6,41 +6,28 @@
     <meta name="viewport" 
           content="width=device-width, initial-scale=1.0">
     <title>RCT</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container">
-        <h1>RCT<br>
-            guys im original i swear</h1>
+    <div class="container object-center">
         <div id="time">
-            <span class="digit" id="hr">
-                00</span>
-            <span class="txt">Hr</span>
-            <span class="digit" id="min">
-                00</span>
+            <span class="digit" id="min">00</span>
             <span class="txt">Min</span>
-            <span class="digit" id="sec">
-                00</span>
+            <span class="digit" id="sec">00</span>
             <span class="txt">Sec</span>
-            <span class="digit" id="count">
-                00</span>
+            <span class="digit" id="count">00</span>
         </div>
         <div id="buttons">
             <button class="btn" id="start">
                 Start</button>
-            <button class="btn" id="stop">
-                Stop</button>
-            <button class="btn" id="reset">
-                Reset</button>
+
         </div>
     </div>
 
-    <script>let startBtn = document.getElementById('start');
-        let stopBtn = document.getElementById('stop');
-        let resetBtn = document.getElementById('reset');
+    <script>
+        let startBtn = document.getElementById('start');
+
         
-        let hour = 00;
         let minute = 00;
         let second = 00;
         let count = 00;
@@ -50,17 +37,11 @@
             stopWatch();
         });
         
-        stopBtn.addEventListener('click', function () {
-            timer = false;
-        });
-        
         resetBtn.addEventListener('click', function () {
             timer = false;
-            hour = 0;
             minute = 0;
             second = 0;
             count = 0;
-            document.getElementById('hr').innerHTML = "00";
             document.getElementById('min').innerHTML = "00";
             document.getElementById('sec').innerHTML = "00";
             document.getElementById('count').innerHTML = "00";
@@ -86,14 +67,10 @@
                     second = 0;
                 }
         
-                let hrString = hour;
                 let minString = minute;
                 let secString = second;
                 let countString = count;
-        
-                if (hour < 10) {
-                    hrString = "0" + hrString;
-                }
+
         
                 if (minute < 10) {
                     minString = "0" + minString;
@@ -107,7 +84,6 @@
                     countString = "0" + countString;
                 }
         
-                document.getElementById('hr').innerHTML = hrString;
                 document.getElementById('min').innerHTML = minString;
                 document.getElementById('sec').innerHTML = secString;
                 document.getElementById('count').innerHTML = countString;
